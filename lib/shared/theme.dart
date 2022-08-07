586e28ee2c8bf0f79f85e50d6c7f4d49f1d7e931
lazlanrafar <lazlanrafar@gmail.com>
2022-08-07 11:52:20 +0700

    add theme color

---
+++ lib/shared/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor = const Color(0xffFAFAFA);
+Color primaryColor = const Color(0xff5C40CC);
+Color blackColor = const Color(0xff1F1449);
+Color whiteColor = const Color(0xffFFFFFF);
+Color grayColor = const Color(0xff9698A9);
+Color greenColor = const Color(0xff0EC3AE);
+Color redColor = const Color(0xffEB70A5);
