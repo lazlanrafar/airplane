2f1ffff189a937d77f2cbcc49b81c0dd859f2767
lazlanrafar <lazlanrafar@gmail.com>
2022-08-07 11:55:24 +0700

    add theme text style

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color backgroundColor = const Color(0xffFAFAFA);
 Color primaryColor = const Color(0xff5C40CC);
@@ -7,3 +8,22 @@ Color whiteColor = const Color(0xffFFFFFF);
 Color grayColor = const Color(0xff9698A9);
 Color greenColor = const Color(0xff0EC3AE);
 Color redColor = const Color(0xffEB70A5);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+);
+TextStyle grayTextStyle = GoogleFonts.poppins(
+  color: grayColor,
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: greenColor,
+);
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: redColor,
+);
+TextStyle blueTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
+);
